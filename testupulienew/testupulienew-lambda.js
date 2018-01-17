91c9a357d7124592c5042d645cb384f65718e7a9
UpulieAdrt <35255575+UpulieAdrt@users.noreply.github.com>
2018-01-17 15:11:57 +0530

    added sns with multiple suscribers

--- testupulienew/testupulienew-lambda.js
+++ testupulienew/testupulienew-lambda.js
@@ -1,7 +1,37 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const sns = new AWS.SNS();
+exports.handler = function (event, context, callback) {
 
 
-	callback(null,'Successfully executed');
+	callback(null, 'Successfully executed');
 	console.log("SUccess!");
+
+	sns.publish({
+		Message: 'this is the message for multiple subscriber types',
+		Subject: 'message subject',
+		MessageAttributes: {
+			'Sender': {
+				DataType: 'String',
+				StringValue: 'upulie'
+			},
+			'Company': {
+				DataType: 'String',
+				StringValue: 'ADRT'
+			},
+			'ID': {
+				DataType: 'Number',
+				StringValue: '001'
+			}
+		},
+		MessageStructure: 'String',
+		TopicArn: 'arn:aws:sns:us-east-1:480964559519:new upulie'
+	}).promise()
+		.then(data => {
+			// your code goes here
+		})
+		.catch(err => {
+			// error handling goes here
+		});
+
+
 }
